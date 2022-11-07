038c1fa0133b4ce67af47c23f13698876d25964b
CCmentorMK <michal.kruczkowski@codecool.com>
2022-11-07 15:15:15 +0100

    rest api

---
+++ src/main/java/com/example/rest_api_wsd/RestApiWsdApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest_api_wsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWsdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiWsdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rest_api_wsd/RestApiWsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_api_wsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWsdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
